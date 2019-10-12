369589e3d66464392bce501e20fe34e1199ae3b9
psylon <stanislav.antunovich@yandex.ru>
2019-10-12 22:12:47 +0300

    removed tests for a wile

--- app/src/test/java/geekbrains/ru/lesson4retrofit/di/TestComponent.java
+++ app/src/test/java/geekbrains/ru/lesson4retrofit/di/TestComponent.java
@@ -3,20 +3,12 @@ package geekbrains.ru.lesson4retrofit.di;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import geekbrains.ru.lesson4retrofit.MainPresenterTest;
-import geekbrains.ru.lesson4retrofit.UserPresenterTest;
 import geekbrains.ru.lesson4retrofit.presenters.MainPresenter;
-import geekbrains.ru.lesson4retrofit.presenters.UsersPresenter;
 
 @Component(modules = {TestModule.class})
 @Singleton
 public interface TestComponent {
     void inject(MainPresenter presenter);
-
-    void inject(MainPresenterTest mainPresenterTest);
-
-    void inject(UsersPresenter presenter);
-    void inject(UserPresenterTest userPresenterTest);
 }
 
 
