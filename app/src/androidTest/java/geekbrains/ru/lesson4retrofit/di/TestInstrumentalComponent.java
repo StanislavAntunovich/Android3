f46be4394e4afb149dbd5d4395efabaf70379d14
psylon <stanislav.antunovich@yandex.ru>
2019-10-12 22:11:41 +0300

    removed tests for a wile

--- app/src/androidTest/java/geekbrains/ru/lesson4retrofit/di/TestInstrumentalComponent.java
+++ app/src/androidTest/java/geekbrains/ru/lesson4retrofit/di/TestInstrumentalComponent.java
@@ -3,17 +3,9 @@ package geekbrains.ru.lesson4retrofit.di;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import geekbrains.ru.lesson4retrofit.data.DataHelper;
-import geekbrains.ru.lesson4retrofit.di.modules.DataModule;
-import geekbrains.ru.lesson4retrofit.di.modules.RetrofitModule;
-import geekbrains.ru.lesson4retrofit.rest.RestAPI;
 
-@Component(modules = {RetrofitModule.class, DataModule.class, ActivityModule.class})
+@Component
 @Singleton
 public interface TestInstrumentalComponent {
-    void inject(DataInstrumentedTest test);
-    void inject(DataHelper dataWorker);
-    void inject(UserDataInstrumentedTest test);
 
-    RestAPI getApi();
 }
